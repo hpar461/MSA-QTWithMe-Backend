e454a3af1219cd0b0aa764d34605ff28d5927af3
hyungsangpark <hpar461@aucklanduni.ac.nz>
2021-09-22 12:07:31 +1200

    Added CORS policy.

--- QTWithMe/Startup.cs
+++ QTWithMe/Startup.cs
@@ -33,6 +33,13 @@ namespace QTWithMe
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            }));
+            
             services.AddPooledDbContextFactory<AppDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
@@ -71,6 +78,8 @@ namespace QTWithMe
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors("MyPolicy");
 
             app.UseHttpsRedirection();
             
